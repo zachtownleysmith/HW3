73473d87e9b0d915f7090ba1082d6e71bc5a47e5
Lori Lee <zachtownleysmith@yahoo.com>
2021-04-04 22:31:56 -0500

    Adding Reporting and TXT file for comments on data

--- company_sent.py
+++ company_sent.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from textblob import TextBlob
+pd.set_option("display.max_rows", 100, "display.max_columns", 4, "display.max_colwidth", 200)
 
 # Read in HW2 files as pandas DFs
 raw_df1 = pd.read_csv('output.csv')
@@ -21,8 +22,17 @@ comb_df['Subjectivity'] = [TextBlob(purpose).sentiment.subjectivity for purpose 
 
 # Sort Data Frame by Sentiment (Polarity then Subjectivity)
 comb_df = comb_df.sort_values(by=['Polarity', 'Subjectivity'])
+comb_df.reset_index(drop=True, inplace=True)
 
+if __name__ == '__main__':
+    # Report 10 companies with highest sentiment
+    print("10 Company Purposes With Highest TextBlob Sentiment:")
+    print(comb_df.tail(10))
+    print("")
 
+    # Report 10 companies with lowest sentiment
+    print("10 Company Purposes With Lowest TextBlob Sentiment:")
+    print(comb_df.head(10))
 
 
 
